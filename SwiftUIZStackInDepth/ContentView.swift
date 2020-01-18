c2c7a140aac8aa14474cdc00ab23bbe05aaabb06
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-18 13:05:57 +0200

    init

--- SwiftUIZStackInDepth/ContentView.swift
+++ SwiftUIZStackInDepth/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("ZStack Example")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                Divider().background(Color.white)
+                ZStack(alignment: .bottomTrailing) {
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundColor(Color.white)
+                        .opacity(0.3)
+                        .frame(width: 150, height: 150)
+                    VStack {
+                        Image(systemName: "house")
+                            .foregroundColor(.white)
+                            .opacity(0.8)
+                        Text("House")
+                            .foregroundColor(.white)
+                            .opacity(0.7)
+                    }
+                    .padding()
+                    .shadow(radius: 10)
+                }
+            }
+        }
     }
 }
 
